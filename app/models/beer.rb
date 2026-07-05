039b4faa2e2c8bc73121fec377146b6edb75817c
kdepeazer <kdepeazer@rrc.ca>
2026-07-05 16:10:44 -0500

    Add model associations including two many-to-many relationships

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,8 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
   belongs_to :style
+  has_many :reviews, dependent: :destroy
+  has_many :users, through: :reviews
+  has_many :beer_hops, dependent: :destroy
+  has_many :hops, through: :beer_hops
 end
--- app/models/hop.rb
+++ app/models/hop.rb
@@ -1,2 +1,4 @@
 class Hop < ApplicationRecord
+  has_many :beer_hops, dependent: :destroy
+  has_many :beers, through: :beer_hops
 end
