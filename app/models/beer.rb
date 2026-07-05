9c4cba1a3f0245733adcde333033647edabd363a
kdepeazer <kdepeazer@rrc.ca>
2026-07-05 16:47:32 -0500

    Normalize line endings

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -5,7 +5,7 @@ class Beer < ApplicationRecord
   has_many :users, through: :reviews
   has_many :beer_hops, dependent: :destroy
   has_many :hops, through: :beer_hops
-
+  
   validates :name, presence: true
   validates :abv, numericality: { greater_than_or_equal_to: 0, less_than: 1 }, allow_nil: true
 end
