68a0f96eaef111685ded8a3f6886ee115cd822f4
kdepeazer <kdepeazer@rrc.ca>
2026-07-09 13:44:21 -0500

    Add graceful 404 handling for missing breweries, beers, and styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+  private
+
+  def render_not_found
+    render html: "<div class='container mt-4'><h1>Page not found</h1><p>Sorry, we couldn't find what you were looking for. <a href='/'>Return home</a>.</p></div>".html_safe, status: :not_found
+  end
 end
